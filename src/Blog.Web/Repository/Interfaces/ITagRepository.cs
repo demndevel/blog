1dd2e21778a63fa59674d88b44f40f1904eb7093
demndevel <demndevel@gmail.com>
2023-01-28 20:26:34 +0500

    Remove tag repository

--- src/Blog.Web/Repository/Interfaces/ITagRepository.cs
+++
@@ -1,9 +0,0 @@
-using Domain.Entities;
-using Domain.Entities.Tag;
-
-namespace Web.Repository.Interfaces;
-
-public interface ITagRepository : IRepository<Tag>
-{
-    List<Note> GetNotesByTag(string tag);
-}
