28b29d6d011623a26bbee143462e24eb5a7743fc
demndevel <demndevel@gmail.com>
2023-02-20 15:30:09 +0500

    Remove Database.EnsureCreated in ApplicationContext.cs

--- src/Blog.Infrastructure/Persistence/ApplicationContext.cs
+++ src/Blog.Infrastructure/Persistence/ApplicationContext.cs
@@ -11,9 +11,7 @@ public sealed class ApplicationContext : DbContext, IApplicationContext
 {
     public ApplicationContext(DbContextOptions<ApplicationContext> options)
         : base(options)
-    {
-        Database.EnsureCreated(); // TODO: Remove this line and replace with migrations
-    }
+    { }
 
     public DbSet<Note> Notes { get; set; } = null!;
     public DbSet<Project> Projects { get; set; } = null!;
